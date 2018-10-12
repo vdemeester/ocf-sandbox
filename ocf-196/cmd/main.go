537a7dcf1db66513f434de34f867c3f63be3926a
Vincent Demeester <vincent@sbr.pm>
2018-10-12 16:50:20 +0200

    add vendors :D
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- ocf-196/cmd/main.go
+++ ocf-196/cmd/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"os"
 
+	"github.com/cbroglie/mustache"
+	newapp "github.com/openshift/origin/pkg/oc/lib/newapp/app"
 	"github.com/spf13/cobra"
 )
 
@@ -14,7 +18,76 @@ var (
 	}
 )
 
+func runCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "run",
+		Short: "run the build and show logs",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("run")
+			return nil
+		},
+	}
+	return cmd
+}
+
+type createOption struct {
+	name        string
+	image       string
+	imageStream string
+	toDocker    bool
+}
+
+func validateOpts(opt createOption) error {
+	if opt.name == "" {
+		return errors.New("name is empty")
+	}
+	if opt.image == "" {
+		return errors.New("image is empty")
+	}
+	if opt.imageStream == "" {
+		return errors.New("image-stream is empty")
+	}
+	return nil
+}
+
+func createCommand() *cobra.Command {
+	var opt createOption
+	cmd := &cobra.Command{
+		Use:   "create",
+		Short: "create or update the build config",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if err := validateOpts(opt); err != nil {
+				return err
+			}
+			config := newapp.NewAppConfig()
+			template, err := ioutil.ReadFile("./spec.mustache")
+			if err != nil {
+				return err
+			}
+			m := map[string]string{
+				"name":        opt.name,
+				"image":       opt.image,
+				"imageStream": opt.imageStream,
+				"toDocker":    fmt.Sprintf("%v", opt.toDocker),
+			}
+			yaml, err := mustache.Render(string(template), m)
+			if err != nil {
+				return err
+			}
+			fmt.Println(yaml)
+			return nil
+		},
+	}
+	cmd.Flags().StringVar(&opt.name, "name", "", "build configuration name")
+	cmd.Flags().StringVar(&opt.image, "image", "", "image name to push")
+	cmd.Flags().StringVar(&opt.imageStream, "image-stream", "", "image stream to use as build input")
+	cmd.Flags().BoolVar(&opt.toDocker, "toDocker", true, "push the image to a docker registry")
+	return cmd
+}
+
 func main() {
+	rootCmd.AddCommand(runCommand())
+	rootCmd.AddCommand(createCommand())
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
