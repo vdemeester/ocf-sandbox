bee5d1f20ce6327030e458a4915a7b0883eddd23
Vincent Demeester <vincent@sbr.pm>
2018-10-19 16:27:54 +0200

    s2i-builder COPY instead of link
    Signed-off-by: Vincent Demeester <vincent@sbr.pm>

--- ocf-230/s2i-builder/create-dockerfile.sh
+++ ocf-230/s2i-builder/create-dockerfile.sh
@@ -9,7 +9,7 @@ echo "Base Image: $baseImage"
 echo "FROM $1" >> ./Dockerfile
 echo "" >> ./Dockerfile
 echo "USER root"
-echo "RUN ln -s /workspace /tmp/src" >> ./Dockerfile
+echo "COPY . /tmp/src" >> ./Dockerfile
 echo "RUN $STI_SCRIPTS_PATH/assemble" >> ./Dockerfile
 echo "USER 1001"
 echo "CMD $STI_SCRIPTS_PATH/run" >> ./Dockerfile
